c44f63e64fcac2cc8bcd91016e894cc8cddbf1b6
Laura Spieker <ljspiek@gmail.com>
2020-09-15 21:59:31 -0500

    added ticking and formatting

--- src/Components/Time.js
+++ src/Components/Time.js
@@ -5,7 +5,7 @@ export default class Time extends Component {
   render() {
     return (
       <div>
-        <Clock />
+        <Clock format="HH:mm:ss" interval={1000} ticking={true} />
       </div>
     );
   }
